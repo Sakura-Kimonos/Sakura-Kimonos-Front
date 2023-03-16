957b0376496038136d6f4083fc550b63738218bf
Ma-shi22 <mashisantos@gmail.com>
2023-03-16 11:18:30 +0100

    add change navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,14 @@
 import React from 'react'
-<<<<<<< HEAD
-//import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
-//import { router } from './router'
-//import { RouterProvider } from 'react-router-dom'
-=======
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import './index.css'
 import { router } from './router/router'
 import { RouterProvider } from 'react-router-dom'
->>>>>>> feature/home
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-<<<<<<< HEAD
-  <RouterProvider router={router} />
-=======
     <RouterProvider router={router} />
->>>>>>> feature/home
   </React.StrictMode>,
 )
