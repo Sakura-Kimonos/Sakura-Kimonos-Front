873ba26153cf42b7f8bb2a3a7e60516523c6cd84
Rocio-Leiva <rmleipec@gmail.com>
2023-03-18 10:35:45 +0100

    added our vision

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+
 import './index.css'
 import { router } from './router/router'
 import { RouterProvider } from 'react-router-dom'
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,22 @@
 import "./styleSheetPages/Home.css"
 import Slide from '../components/Slide';
 import Navbar from '../components/Navbar'
+import OurVision from "../components/OurVision";
 // import {  useLoaderData } from 'react-router-dom'
 // import Trending from "../components/Trending";
+
 const Home = () => {
     // const {products}  = useLoaderData();
     return (
 <>
-<section className="container-fluid">
+<section className="hero">
                 <Navbar/>
                 <Slide/>
                 <h2 className="title">LAST PRODUCTS</h2>
             <div className="anuncios">
                 {/* <Trending products={products}/> */}
             </div>
+            <OurVision />
             </section>
                 </>
     )
