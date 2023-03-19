e35a80759cd6a74df927135d64412eadb5d89fee
Rocio-Leiva <rmleipec@gmail.com>
2023-03-19 13:52:06 +0100

    added DashboardAdm

--- src/components/CardAdm.jsx
+++ src/components/CardAdm.jsx
@@ -1,24 +1,57 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import {BsSearchHeart, BsPencilSquare, BsTrash } from 'react-icons/bs';
+import {BsSearchHeart, BsCart3} from 'react-icons/bs';
+import { useLoaderData } from "react-router-dom";
 
-function CardAdm() {
-  return (
-    <Card border="light" style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="src\assets\haori1.png" />
-      <Card.Body className='text-center'>
-        <Card.Title>Dark Tôri - 50€ </Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Haori</Card.Subtitle>
-        {/* <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text> */}
-        <Button variant="light"><BsSearchHeart/> View </Button>
-        <Button variant="light"><BsPencilSquare/> Edit </Button>
-        <Button variant="light"><BsTrash/> Delete </Button>
-      </Card.Body>
-    </Card>
-  );
-}
 
-export default CardAdm; 
+const CardUser = () => {
+  const { CardUser } = useLoaderData();
+
+return (
+  <Card border="light" style={{ width: '18rem' }}>
+         <Card.Img variant="top" src={CardUser.img}  />
+         <Card.Body className='text-center'>
+           <Card.Title>{CardUser.title} </Card.Title>
+           <Card.Subtitle className="mb-2 text-muted">{CardUser.price}</Card.Subtitle>
+           {/* <Card.Text>
+  //           Some quick example text to build on the card title and make up the
+  //           bulk of the card's content.
+  //         </Card.Text> */}
+         <Button variant="light"><BsSearchHeart/> View </Button>
+         <Button variant="light"><BsPencilSquare/> Edit </Button>
+           <Button variant="light"><BsCart3/> Add to cart </Button>
+        </Card.Body>
+       </Card>
+)
+};
+  export default CardUser;
+
+
+
+
+
+
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
+// import {BsSearchHeart, BsPencilSquare, BsTrash } from 'react-icons/bs';
+
+// function CardAdm() {
+//   return (
+//     <Card border="light" style={{ width: '18rem' }}>
+//       <Card.Img variant="top" src="src\assets\haori1.png" />
+//       <Card.Body className='text-center'>
+//         <Card.Title>Dark Tôri - 50€ </Card.Title>
+//         <Card.Subtitle className="mb-2 text-muted">Haori</Card.Subtitle>
+//         {/* <Card.Text>
+//           Some quick example text to build on the card title and make up the
+//           bulk of the card's content.
+//         </Card.Text> */}
+//         <Button variant="light"><BsSearchHeart/> View </Button>
+//         <Button variant="light"><BsPencilSquare/> Edit </Button>
+//         <Button variant="light"><BsTrash/> Delete </Button>
+//       </Card.Body>
+//     </Card>
+//   );
+// }
+
+// export default CardAdm; 
