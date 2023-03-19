7260c604019d533fa8470dfa95d64a6f5d690a57
Rocio-Leiva <rmleipec@gmail.com>
2023-03-19 10:37:27 +0100

    added media queris last products

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,40 +1,43 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import {BsSearchHeart, BsCart3} from 'react-icons/bs';
-import './styleSheet/Trending.css'
-
+import { BsSearchHeart, BsCart3 } from 'react-icons/bs';
+import './styleSheet/Trending.css';
 import { Link } from 'react-router-dom';
 
 function Trending({ products, deleteProduct }) {
-  
   const randomIndex = Math.floor(Math.random() * (products.length - 3));
   const resultTrending = products.slice(randomIndex, randomIndex + 3);
 
   return (
-    <div className="cards d-flex flex-row justify-content-evenly align-items-center">
-
-      {resultTrending.map(product => {
+    <div className="cards row justify-content-center">
+      {resultTrending.map((product) => {
         return (
-            <Card key={product.id} className="card" border="light" style={{ width: '18rem' }}>
-                  <Card.Img variant="top" src={product.img}  />
-                  <Card.Body className='text-center'>
-                   <Card.Title>{product.title} </Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">{product.price}</Card.Subtitle>
-                    {/* <Card.Text>
-                      Some quick example text to build on the card title and make up the
-                      bulk of the card's content.
-                    </Card.Text> */}
-                    <Button variant="light"><BsSearchHeart/> View </Button>
-                    <Button variant="light"><BsCart3/> Add to cart </Button>
-                  </Card.Body>   
-              <Link to={`/userDashboard/${product.id}`}><Button className='btn-ad' id="btn-edit">More information</Button></Link>
-              </Card>
-
-        )
+          <div className="col-10 col-sm-6 col-md-4 col-lg-3 mb-3" key={product.id}>
+            <Card className="card" border="light">
+              <Card.Img variant="top" src={product.img} />
+              <Card.Body className="text-center">
+                <Card.Title>{product.title}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{product.price}</Card.Subtitle>
+                <Button variant="light">
+                  <BsSearchHeart /> View
+                </Button>
+                <Button variant="light">
+                  <BsCart3 /> Add to cart
+                </Button>
+              </Card.Body>
+              <Link to={`/userDashboard/${product.id}`}>
+                <Button className="btn-ad" id="btn-edit">
+                  More information
+                </Button>
+              </Link>
+            </Card>
+          </div>
+        );
       })}
     </div>
   );
 }
 
 export default Trending;
+
