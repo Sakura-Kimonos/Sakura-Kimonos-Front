44a3f6b49db3d37579809f9306b999f8c2de64ec
Rocio-Leiva <rmleipec@gmail.com>
2023-03-21 12:37:44 +0100

    fixed contactus

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import Modal from "react-modal";
 import '../pages/styleSheetPages/ContactUs.css'
 import Navbar from '../components/Navbar'
 
 
 function ContactForm() {
   const [message, setMessage] = useState("");
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+  
 
   const handleTextareaChange = (event) => {
     setMessage(event.target.value);
@@ -23,15 +22,10 @@ function ContactForm() {
     window.location.reload();
   };
 
-  const closeModal = () => {
-    setModalIsOpen(false);
-    setMessage("");
-  };
-
   return (
     <>
-    {/* <Navbar/> */}
-    <div className="text-container-contact">
+    <Navbar/>
+    <div className="text-container-contact ">
       <form className="form-contact" onSubmit={handleFormSubmit}>
         <h2 className="titleContact">Contact with us</h2>
         <label>
@@ -44,20 +38,7 @@ function ContactForm() {
         <button className="btn btn-primary" id="btn-ad" type="submit">
         Send Message
         </button>
-        {/* <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        className="modal"
-        overlayClassName="overlay"
-      >
-        <h2 className="modal-title">Message Sent</h2>
-        <p className="modal-message">
-        Your message has been sent successfully. We will reply to you as soon as possible.
-        </p>
-        <button className="modal-close-btn" onClick={closeModal}>
-          Close
-        </button>
-      </Modal> */}
+        
       </form>
       
     </div>
