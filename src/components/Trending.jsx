fab9eeb69e8015eabc4b2d6658bf91ff31785a31
Rocio-Leiva <rmleipec@gmail.com>
2023-03-23 19:44:59 +0100

    added resposive

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -5,6 +5,7 @@ import { BsSearchHeart, BsCart3 } from 'react-icons/bs';
 import './styleSheet/Trending.css';
 import React, {useState} from 'react';
 import ProductModalUser from './ProductModalUser'; 
+import '../pages/styleSheetPages/userDashboard.css'
 
 
 
@@ -23,26 +24,23 @@ function Trending({ products}) {
 
   return (
     <>
-    <div className="cards row justify-content-center">
+    <div className="cards">
        <h2 id="last-products"className="title">LAST PRODUCTS</h2>
       {productList.map((product) => {
         return (
           <>
             <ProductModalUser show={show} handleClose={handleClose} product={productModal} />
-              <div className="col-10 col-sm-6 col-md-4 col-lg-3 mb-3" key={product.id}>
-                <Card className="card" border="light">
-                  <Card.Img variant="top" src={product.img} />
-                  <Card.Body className="text-center">
-                    <Card.Title>{product.title}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">${product.price}</Card.Subtitle>
-                    <Button variant="light" onClick={() => handleShow(product.id)}>
-                      <BsSearchHeart /> View
-                    </Button>
-                    <Button variant="light">
-                      <BsCart3 /> Add to cart
-                    </Button>
-                  </Card.Body>
-                </Card>
+            <div key={product.id}>
+
+            <Card border="light" style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={product.img}  />
+                    <Card.Body className='text-center'>
+                      <Card.Title>{product.title} </Card.Title>
+                      <Card.Subtitle className="mb-2 text-muted">${product.price}</Card.Subtitle>
+                    <Button variant="light" onClick={() => handleShow(product.id)}><BsSearchHeart/> View </Button>
+                    <Button variant="light"><BsCart3/> Add to cart </Button>
+                    </Card.Body>
+              </Card>
               </div>
           </>
         );
--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -98,20 +98,3 @@ export default Dashboard;
 
 
 
-
-
-// import CardsUser from '../components/CardsUser';
-
-// // import {  useLoaderData } from 'react-router-dom'
-// // import Trending from "../components/Trending";
-// const UserDashboard = () => {
-//     // const {products}  = useLoaderData();
-//     return (
-// <>
-
-//                 <CardsUser/>
-          
-//                 </>
-//     )
-// };
-// export default UserDashboard;
