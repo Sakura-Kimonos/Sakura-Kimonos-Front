17af12825b5b682d9080f325f9d0531720187797
Rocio-Leiva <rmleipec@gmail.com>
2023-03-23 10:33:15 +0100

    added buttonNewProduct

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,102 @@
-import Logo from '../assets/LogoSakura1.png';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import { FaShoppingCart } from 'react-icons/fa';
+import React, { useState } from 'react';
+import {
+  MDBNavbar,
+  MDBContainer,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBCollapse,
+ 
+  MDBIcon,
+  MDBNavbarNav,
+  
+} from 'mdb-react-ui-kit';
 
-function MyNavbar() {
+export default function App() {
+  const [showNavNoTogglerSecond, setShowNavNoTogglerSecond] = useState(false);
+
   return (
-    <Container fluid className='menu'>
-    <Navbar bg="light" expand="md">
-     
-        <Navbar.Brand as={Link} to="/">
-        <img src={Logo} alt="Sakura-Kimonos"className='Logo'/>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbar-nav" />
-        <Navbar.Collapse id="navbar-nav">
-          <Nav className="ms-auto justify-content-end" style={{ maxHeight: '100px' }}>
-          <Nav.Link as={Link} to="/" className="mx-4">HOME</Nav.Link>
-           <Nav.Link as={Link} to="/userDashboard">PRODUCTS</Nav.Link>
-           <Nav.Link as={Link} to="/join" className="mx-4">JOIN</Nav.Link>
-           <Nav.Link as={Link} to="/login" className="mx-4">LOGIN</Nav.Link>
-           <Nav.Link as={Link} to="/ShoppingCart" className="mx-4"><FaShoppingCart /></Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-    
-    </Navbar>
-    </Container>
+    <>
+      <MDBNavbar expand='lg' light bgColor='light'>
+        <MDBContainer fluid>
+          <MDBNavbarBrand href='#'>Navbar</MDBNavbarBrand>
+          <MDBNavbarToggler
+            type='button'
+            data-target='#navbarTogglerDemo02'
+            aria-controls='navbarTogglerDemo02'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+            onClick={() => setShowNavNoTogglerSecond(!showNavNoTogglerSecond)}
+          >
+            <MDBIcon icon='bars' fas />
+          </MDBNavbarToggler>
+          <MDBCollapse navbar show={showNavNoTogglerSecond}>
+            <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current='page' href='#'>
+                  Home
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#'>Link</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink disabled href='#' tabIndex={-1} aria-disabled='true'>
+                  Disabled
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+            </MDBNavbarNav>
+            
+          </MDBCollapse>
+        </MDBContainer>
+      </MDBNavbar>
+    </>
   );
 }
 
-export default MyNavbar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import Logo from '../assets/LogoSakura1.png';
+// import { Navbar, Nav, Container } from 'react-bootstrap';
+// import { Link } from 'react-router-dom';
+// import { FaShoppingCart } from 'react-icons/fa';
+
+// function MyNavbar() {
+//   return (
+//     <Container fluid className='menu'>
+//     <Navbar bg="light" expand="md">
+     
+//         <Navbar.Brand as={Link} to="/">
+//         <img src={Logo} alt="Sakura-Kimonos"className='Logo'/>
+//         </Navbar.Brand>
+//         <Navbar.Toggle aria-controls="navbar-nav" />
+//         <Navbar.Collapse id="navbar-nav">
+//           <Nav className="ms-auto justify-content-end" style={{ maxHeight: '100px' }}>
+//           <Nav.Link as={Link} to="/" className="mx-4">HOME</Nav.Link>
+//            <Nav.Link as={Link} to="/userDashboard">PRODUCTS</Nav.Link>
+//            <Nav.Link as={Link} to="/join" className="mx-4">JOIN</Nav.Link>
+//            <Nav.Link as={Link} to="/login" className="mx-4">LOGIN</Nav.Link>
+//            <Nav.Link as={Link} to="/ShoppingCart" className="mx-4"><FaShoppingCart /></Nav.Link>
+//           </Nav>
+//         </Navbar.Collapse>
+    
+//     </Navbar>
+//     </Container>
+//   );
+// }
+
+// export default MyNavbar;
--- src/pages/AdmDashboard.jsx
+++ src/pages/AdmDashboard.jsx
@@ -1,20 +1,21 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import {  useLoaderData } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 import { productHandler } from "../handlers/productHandler";
-import {BsSearchHeart,  BsCart3, BsTrash} from 'react-icons/bs';
+import { BsSearchHeart, BsCart3, BsTrash } from 'react-icons/bs';
 import '../pages/styleSheetPages/AdmDashboard.css'
 import SideBar from '../components/SideBar';
 import ProductModalAdm from "../components/ProductModalAdm";
+import ButtonNewProduct from '../components/ButtonNewProduct';
 
 function Dashboard() {
- 
+
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
   };
-  const { products} = useLoaderData();
+  const { products } = useLoaderData();
   const [productsData, setProductsData] = useState(products);
   const [searchQuery, setSearchQuery] = useState("");
   const data = productsData.filter(
@@ -25,55 +26,58 @@ function Dashboard() {
   const deleteProduct = async (id) => {
     await productHandler.deleteProduct(id);
     setProductsData(productsData.filter(post => post.id !== id))
-}
+  }
 
-const [show, setShow] = useState(false);
-const [productModal, setProductModal] = useState({});
-const handleClose = () => setShow(false);
-const handleShow = (productId) => {
-  setProductModal(productsData.find(product => product.id == productId));
-  setShow(true)
-};
+  const [show, setShow] = useState(false);
+  const [productModal, setProductModal] = useState({});
+  const handleClose = () => setShow(false);
+  const handleShow = (productId) => {
+    setProductModal(productsData.find(product => product.id == productId));
+    setShow(true)
+  };
 
   return (
     <>
-    <SideBar/>
+      <SideBar />
       <div className="container-gn" id="text">
-      <br />
-      <h1> Sakura Kimonos</h1>
-    <>
-      <div className="container-bar">
-        <input
-          className="searchStyle"
-          type="text"
-          placeholder="🔍 Search by product name or price"
-          value={searchQuery}
-          onChange={handleSearchChange}
-        />
-      </div>
-      <div className="cards">
-        {data.map((product) => {
-         return (
-          <>
-            <ProductModalAdm show={show} handleClose={handleClose} product={productModal} />
-              <Card border="light" style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src={product.img}  />
+        <br />
+        <h1> Sakura Kimonos</h1>
+        <>
+          <div className="container-bar">
+            <input
+              className="searchStyle"
+              type="text"
+              placeholder="🔍 Search by product name or price"
+              value={searchQuery}
+              onChange={handleSearchChange}
+            />
+          </div>
+
+          <ButtonNewProduct />
+
+          <div className="cards">
+            {data.map((product) => {
+              return (
+                <>
+                  <ProductModalAdm show={show} handleClose={handleClose} product={productModal} />
+                  <Card border="light" style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={product.img} />
                     <Card.Body className='text-center'>
                       <Card.Title>{product.title} </Card.Title>
                       <Card.Subtitle className="mb-2 text-muted">${product.price}</Card.Subtitle>
-                    <Button variant="light" onClick={() => handleShow(product.id)}><BsSearchHeart/> View </Button>
-                    <Button variant="light"><BsCart3/> Add to cart </Button>
-<Button variant="light"><BsTrash/> Delete </Button>
+                      <Button variant="light" onClick={() => handleShow(product.id)}><BsSearchHeart /> View </Button>
+                      <Button variant="light"><BsCart3 /> Add to cart </Button>
+                      <Button variant="light"><BsTrash /> Delete </Button>
 
                     </Card.Body>
-              </Card>
-          </>
-          )
-        })}
+                  </Card>
+                </>
+              )
+            })}
+          </div>
+        </>
       </div>
     </>
-    </div>
-  </>
   );
 }
 
