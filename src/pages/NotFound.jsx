fb60258edba14d60aafffbe5393614dee30aa9fd
Rocio-Leiva <rmleipec@gmail.com>
2023-03-24 09:30:32 +0100

    changed notfound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -11,13 +11,12 @@ const NotFound = () => {
     return (
       <div className="column">
         <h1>404</h1>
-        <p>Page not found</p>
+        <span role="img" aria-label="sad face" style={{fontSize: "30px"}}>😔</span>
         <p>{error.statusText || error.message}</p>
-        <span role="img" aria-label="sad face" style={{fontSize: "48px"}}>😔</span>
-
+        <SakuraAnimation /> 
         <Link to="/">Please click here to return to the home page</Link>
         <br></br>
-        <SakuraAnimation /> 
+      
         <img src="src/assets/LogoSakura1.png" alt="logo" />
       </div>
     );
