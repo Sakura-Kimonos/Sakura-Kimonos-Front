6fdd225bb16da400fea25f9fe889a2023ba90ef9
Rocio-Leiva <rmleipec@gmail.com>
2023-03-26 12:55:08 +0200

    added delete modal

--- src/pages/AdmDashboard.jsx
+++ src/pages/AdmDashboard.jsx
@@ -4,35 +4,44 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { useLoaderData } from "react-router-dom";
 import { productHandler } from "../handlers/productHandler";
-import { BsSearchHeart, BsPencilSquare, BsTrash } from 'react-icons/bs';
+import { BsSearchHeart, BsPencilSquare, BsTrash, BsX } from 'react-icons/bs';
 import '../pages/styleSheetPages/AdmDashboard.css'
 import SideBar from '../components/SideBar';
 import ProductModalAdm from "../components/ProductModalAdm";
 import ButtonNewProduct from '../components/ButtonNewProduct';
+import Modal from 'react-bootstrap/Modal';
 
 function Dashboard() {
-
-
   const { products } = useLoaderData();
   const [productsData, setProductsData] = useState(products);
   const [searchQuery, setSearchQuery] = useState("");
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
- };
+  };
   const data = productsData.filter((product) => {
     return product.title.toLowerCase().includes(searchQuery.toLowerCase()) || product.price == searchQuery;
   });
+
   const deleteProduct = async (id) => {
     await productHandler.deleteProduct(id);
-    setProductsData(productsData.filter(post => post.id !== id))
-  }
+    setProductsData(productsData.filter(product => product.id !== id));
+    handleCloseConfirmation();
+  };
 
   const [show, setShow] = useState(false);
   const [productModal, setProductModal] = useState({});
   const handleClose = () => setShow(false);
   const handleShow = (productId) => {
     setProductModal(productsData.find(product => product.id == productId));
-    setShow(true)
+    setShow(true);
+  };
+
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [productIdToDelete, setProductIdToDelete] = useState(null);
+  const handleCloseConfirmation = () => setShowConfirmation(false);
+  const handleShowConfirmation = (productId) => {
+    setProductIdToDelete(productId);
+    setShowConfirmation(true);
   };
 
   return (
@@ -57,7 +66,7 @@ function Dashboard() {
           <div className="cards">
             {data.map((product) => {
               return (
-                <>
+                <React.Fragment key={product.id}>
                   <ProductModalAdm show={show} handleClose={handleClose} product={productModal} />
                   <Card border="light" style={{ width: '18rem' }}>
                     <Card.Img variant="top" src={product.img} />
@@ -65,20 +74,33 @@ function Dashboard() {
                       <Card.Title>{product.title} </Card.Title>
                       <Card.Subtitle className="mb-2 text-muted">${product.price}</Card.Subtitle>
                       <Button variant="light" onClick={() => handleShow(product.id)}><BsSearchHeart /> View </Button>
-                      <Button variant="light"><BsPencilSquare/> Edit </Button>
-                      <Button variant="light" onClick={() =>deleteProduct(product.id)}><BsTrash/> Delete </Button>
+                      <Button variant="light"><BsPencilSquare /> Edit </Button>
+                      <Button variant="light" onClick={() => handleShowConfirmation(product.id)}><BsTrash /> Delete </Button>
                     </Card.Body>
                   </Card>
-                </>
+                </React.Fragment>
               )
             })}
           </div>
+
+          <Modal show={showConfirmation} onHide={handleCloseConfirmation} className="my-modal">
+            <Modal.Header closeButton>
+              <Modal.Title>Confirm Deletion</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Are you sure you want to delete this product?</Modal.Body>
+            <Modal.Footer>
+              <Button variant="light" onClick={() => handleCloseConfirmation()} className="my-button"><BsX /> Cancel </Button>
+              <Button variant="light" onClick={() => deleteProduct(productIdToDelete)} className="my-button"><BsTrash /> Delete </Button>
+            </Modal.Footer>
+          </Modal>
         </>
       </div>
     </>
   );
+
 }
 
-export default Dashboard;
 
+
+export default Dashboard;
 
