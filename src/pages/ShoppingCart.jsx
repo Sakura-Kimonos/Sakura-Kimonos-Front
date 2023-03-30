89e91ba276181a788ddd83a7a70c09923476c35c
Ma-shi22 <mashisantos@gmail.com>
2023-03-30 13:35:50 +0200

    add card to pay

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -5,7 +5,11 @@ import Card from 'react-bootstrap/Card';
 import '../pages/styleSheetPages/ShoppingCart.css'
 import Navbar from '../components/Navbar';
 import { BsSearchHeart,  BsTrash, BsX } from 'react-icons/bs';
-
+import visa from '../assets/visa.png';
+import paypal from '../assets/paypal.jpg';
+import mastercard from '../assets/mastercard.png';
+import gpay from '../assets/Gpay.jpg';
+import { Row, Col, Container } from 'react-bootstrap';
 
 const Cart = () => {
     
@@ -27,11 +31,6 @@ const Cart = () => {
         return total + product.price;
         }, 0);
 
-/*const total = items.reduce((acc, item) => acc + item.price * item.quantity, 0);*/
-/*const removeItem = item => {
-    setItems(prevItems => prevItems.filter(i => i.id !== item.id));
-    };*/
-
 
 return (
     <>
@@ -43,30 +42,50 @@ return (
 
     <div className="cards" >
     {cartProducts.map((product) => (
-     <React.Fragment key={product.id}>
-     <Card border="light" style={{ width: '18rem' }}>
-       <Card.Img variant="top" src={product.img} />
-       <Card.Body className='text-center'>
-         <Card.Title>{product.title} </Card.Title>
-         <Card.Subtitle className="mb-2 text-muted">${product.price}</Card.Subtitle>
-         <Button variant="outline-primary" style={{width: '100px'}}  onClick={() => handleRemove(product.id)}><BsTrash />Delete</Button>
-       </Card.Body>
-     </Card>
-   </React.Fragment>
+        <React.Fragment key={product.id}>
+        <Card border="light" style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={product.img} />
+        <Card.Body className='text-center'>
+            <Card.Title>{product.title} </Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">${product.price}</Card.Subtitle>
+            <Button variant="outline-primary" style={{width: '100px'}}  onClick={() => handleRemove(product.id)}><BsTrash />Delete</Button>
+        </Card.Body>
+        </Card>
+    </React.Fragment>
         ))}
-       <div className="container-bar" id="text">
+        
+        
+    
+        <Card style={{backgroundColor: '#D6C6B4', border: ' none', borderRadius: '10px', textAlign: 'center'}}>
+        
+        <div className="container-bar" id="text">
         <br />
         <br />   
-        <h3>Amount to Pay</h3>
-        <h3>{`Total: ${totalPrice}`}$</h3>
+        <h3></h3>
+        <h3>{`Total: ${totalPrice}`}$</h3> 
+        <h6>*Taxes included</h6> 
+        <br />  
+        <Button className='buy' type="submit" id="btn" style={{width: '100px'}} >To Pay</Button>
         <br />
         </div>
+        <Card.Title style={{marginBottom: '15px', marginTop: '20px', backgroundColor: '#D6C6B4'}} >You can pay with:</Card.Title>
         <br />
-        <Button className='buy' type="submit" id="btn" style={{width: '100px'}} >To Buy</Button>
+        
+            <Row  >
+                <Col ><img src={visa} alt="visa"  style={{height: 60, marginTop: '10px'}}/></Col>
+                <Col ><img src={paypal} alt="paypal" style={{height: 60, marginTop: '12px'}}/></Col>
+                <Col ><img src={gpay} alt="gpay" style={{height: 60, marginTop: '20px'}}/></Col>
+                <Col><img src={mastercard} alt="mastercard" style={{height: 60, marginTop: '20px'}}/></Col>
+                </Row>
+        </Card>
+            
+        
+    </div>
         <br />
         <br />
     </div>    
-</div>
+
+
 </>
 );
 };
